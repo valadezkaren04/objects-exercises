128415ff6ca177eca2c4c7adf079687fe0d725e8
Karen Valadez <valadez.karen04@gmail.com>
2021-04-27 23:29:33 -0500

    finished first loop exercise

--- js/objects-exercise.js
+++ js/objects-exercise.js
@@ -1,16 +1,23 @@
 "use strict";
 //1
-function makeCar(make, model) {
-    return {
-        make: make,
-        model: model
-    }
-}
+// function makeCar(make, model) {
+//     return {
+//         make: make,
+//         model: model
+//     }
+// }
 
 //2
-var salesReport = {
-    title:
-    date:
-    office:
-    employees:
+// var salesReport = {
+//     title:
+//     date:
+//     office:
+//     employees:
+// }
+
+function showMultiplicationTable(num) {
+    for (var i = 0; i <= 10; i++) {
+       console.log(num + " * " + i + " = " + num * i);
+    }
 }
+console.log(7);
