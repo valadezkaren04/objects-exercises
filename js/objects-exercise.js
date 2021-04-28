612291d0a91ffbaf3bfbe0c9f9243d4e212ed27d
Karen Valadez <valadez.karen04@gmail.com>
2021-04-27 22:42:16 -0500

    working on exercises

---
+++ js/objects-exercise.js
@@ -0,0 +1,16 @@
+"use strict";
+//1
+function makeCar(make, model) {
+    return {
+        make: make,
+        model: model
+    }
+}
+
+//2
+var salesReport = {
+    title:
+    date:
+    office:
+    employees:
+}
